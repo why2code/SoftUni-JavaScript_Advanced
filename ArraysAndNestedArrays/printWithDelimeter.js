27be98553d5cd2fc92fb5919e4436af59a1b9638
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-18 12:57:22 +0300

    01. Print with Delimeter

---
+++ ArraysAndNestedArrays/printWithDelimeter.js
@@ -0,0 +1,20 @@
+function printer(arr, delimeter){
+    console.log(arr.join(delimeter));
+}
+
+printer(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+);
+
+printer(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+);
