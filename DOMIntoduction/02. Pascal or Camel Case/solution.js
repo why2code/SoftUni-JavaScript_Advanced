32a0e2f3c6f1dbfc61c5dc8a6629420b57c5a113
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-24 12:34:49 +0300

    02. Pascal or Camel Case

--- DOMIntoduction/02. Pascal or Camel Case/solution.js
+++ DOMIntoduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  let textToModify = (document.getElementById("text").value).toLowerCase();
+  let modifier = document.getElementById("naming-convention").value;
+
+
+  let resultingString = "";
+  let iteratorForLetters = Array.from(textToModify.split(' '));
+
+  if (modifier === "Camel Case") {
+    resultingString += iteratorForLetters[0];
+    for (let i = 1; i < iteratorForLetters.length; i++) {
+      let currWord = iteratorForLetters[i].substring(0, 1).toUpperCase() + iteratorForLetters[i].substring(1);
+      resultingString += currWord;
+    }
+  }
+  else if (modifier === "Pascal Case") {
+    for (let i = 0; i < iteratorForLetters.length; i++) {
+      let currWord = iteratorForLetters[i].substring(0, 1).toUpperCase() + iteratorForLetters[i].substring(1);
+      resultingString += currWord;
+    }
+  }
+  else {
+    resultingString = "Error!";
+  }
+
+  document.getElementById("result").innerHTML = resultingString;
+  
+
 }
