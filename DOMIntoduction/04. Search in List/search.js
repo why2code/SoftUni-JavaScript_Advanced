8b1fba3e8665b28ad9eee1d57be0d50159518dd6
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-24 14:35:49 +0300

    06. Format the Text

--- DOMIntoduction/04. Search in List/search.js
+++ DOMIntoduction/04. Search in List/search.js
@@ -1,3 +1,7 @@
 function search() {
-   // TODO
+   let items = document.getElementById("towns").textContent;
+   let queryText = document.getElementById("searchText").value;
+
+  
+
 }
--- DOMIntoduction/06. Format the Text/solution.js
+++ DOMIntoduction/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let textInput = document.getElementById("input").value;
+
+  let sentencesToFormat = Array.from(textInput.split('.')).filter(n => n);
+
+  let result = "";
+
+  if (sentencesToFormat.length > 0) {
+    result = `<p> ${sentencesToFormat[0]}`;
+  }
+
+  for (let i = 1; i < sentencesToFormat.length; i++) {
+    if (i % 3 === 0) {
+      result += `</p>`;
+      result += `<p>`;
+    }
+    result += `${sentencesToFormat[i]}.`;
+  }
+
+  document.getElementById("output").innerHTML = result;
+
 }
