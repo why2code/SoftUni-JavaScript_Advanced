63985efba502bbd67b6bce5f50d202183f471ba3
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-24 13:18:09 +0300

    03. Accordion

--- DOMIntoduction/03. Accordion/accordion.js
+++ DOMIntoduction/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.getElementsByClassName("button")[0].textContent;
+    let divBlockElement = document.getElementById("extra");
+
+    if(buttonElement === "More"){
+        divBlockElement.style.display = "block";
+        document.getElementsByClassName("button")[0].innerText = "Less"
+    }
+    
+    if(buttonElement === "Less"){
+        divBlockElement.style.display = "none";
+        document.getElementsByClassName("button")[0].innerText = "More"
+    }
 }
