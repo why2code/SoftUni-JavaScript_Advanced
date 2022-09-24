8810a21ab0794ec8235ba34cc8870ea83ca289b5
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-24 19:16:12 +0300

    04. Search in List - solution completed

--- DOMIntoduction/04. Search in List/search.js
+++ DOMIntoduction/04. Search in List/search.js
@@ -1,7 +1,19 @@
 function search() {
-   let items = document.getElementById("towns").textContent;
-   let queryText = document.getElementById("searchText").value;
-
-  
+   let townList = Array.from(document.querySelectorAll("ul li"));
+   let searchText = document.getElementById("searchText").value;
+   let count = 0;
+   for (let i = 0; i < townList.length; i++) {
+      let text = townList[i].textContent;
+      if (text.includes(searchText)) {
+         townList[i].style.textDecoration = "underline";
+         townList[i].style.fontWeight = "bold";
+         count++;
+      }
+      else {
+         townList[i].style.textDecoration = "none";
+         townList[i].style.fontWeight = "normal";
+      }
+   }
+   document.getElementById("result").innerText = `${count} matches found`; 
 
 }
