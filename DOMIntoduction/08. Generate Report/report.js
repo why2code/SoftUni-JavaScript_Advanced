76f371e5fb2d971ee8b149a91c1f008ade87b8af
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-09-24 23:14:39 +0300

    08. Generate Report

--- DOMIntoduction/08. Generate Report/report.js
+++ DOMIntoduction/08. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+    let tableRow = document.querySelectorAll("tbody tr");
+    let tableHead = document.querySelectorAll("thead tr th");
+
+    let res = [];
+
+    for(let i = 0; i < tableRow.length; i++){
+        let tempData = {};
+        let tableData = tableRow[i].cells;
+        for(let x = 0; x < tableData.length; x++){
+            let infoForHeader = tableHead[x].childNodes;
+            if(infoForHeader[1].checked){
+                tempData[infoForHeader[0].textContent.trim().toLocaleLowerCase()] = tableData[x].textContent;
+            }
+        }
+        res.push(tempData);
+    }
+
+    let jsonRes = JSON.stringify(res);
+    document.getElementById("output").textContent = jsonRes;
+
 }
