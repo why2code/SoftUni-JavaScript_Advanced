2b671c04e1b794426c4e9c7e3d7a390be3919cb8
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-01 12:47:00 +0300

    05. Encode and Decode Messages

--- DOM_ManipulationAndEvents/05. Encode-and-Decode-Messages/app.js
+++ DOM_ManipulationAndEvents/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let buttons = Array.from(document.getElementsByTagName("button"));
+
+    buttons[0].addEventListener("click", EncyptMessage);
+    buttons[1].addEventListener("click", DecryptMessage);
+
+    let arrAreas = Array.from(document.getElementsByTagName("textarea"));
+    let inputTextArea = arrAreas[0];
+    let outputTextArea = arrAreas[1];
+    let originalMessageBufffer = "";
+
+    function EncyptMessage() {
+        let textToEncrypt = inputTextArea.value;
+        originalMessageBufffer = textToEncrypt;
+
+        let outputEncryptedText = "";
+        for (let i = 0; i < textToEncrypt.length; i++) {
+            let newLetter = String.fromCharCode(textToEncrypt[i].charCodeAt(0) + 1);
+            outputEncryptedText += newLetter;
+        }
+
+        outputTextArea.value = outputEncryptedText;
+        inputTextArea.value = "";
+    }
+
+    
+
+    function DecryptMessage() {
+        outputTextArea.value = originalMessageBufffer;
+    }
+
+
 }
