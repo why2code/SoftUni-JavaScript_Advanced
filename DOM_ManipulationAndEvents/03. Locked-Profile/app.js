443f8a836a95bd34cc35f3da008cb3d54ee272d9
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-01 11:36:27 +0300

    03. Locked Profile

--- DOM_ManipulationAndEvents/03. Locked-Profile/app.js
+++ DOM_ManipulationAndEvents/03. Locked-Profile/app.js
@@ -1,3 +1,35 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll("button"));
+    for (let button of buttons) {
+        button.addEventListener("click", showOrHideInformation);
+    }
+
+    function showOrHideInformation() {
+        let checkForLockedUnlockedProfile = event.target.parentElement
+            .querySelector("input[type=radio]:checked").value;
+
+        if (checkForLockedUnlockedProfile === "lock" &&
+            event.target.textContent === "Show more") {
+            return;
+        }
+        else if (checkForLockedUnlockedProfile === "lock" &&
+            event.target.textContent === "Hide it") {
+            return;
+        }
+        else {
+            event.target.disabled = false;
+
+            if (event.target.textContent === "Hide it") {
+                event.target.parentElement.querySelector("div").style.display = "";
+                event.target.textContent = "Show more";
+            }
+            else {
+                event.target.parentElement.querySelector("div").style.display = "block";
+                event.target.textContent = "Hide it";
+            }
+        }
+
+
+    }
+
 }
