c31263f8bdca8adc82f17dfeafd6f260f9e4d1f7
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-01 10:27:16 +0300

    02. Time Converter

--- DOM_ManipulationAndEvents/02. Time-Converter/solution.js
+++ DOM_ManipulationAndEvents/02. Time-Converter/solution.js
@@ -1,4 +1,44 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll("input[type=button]"));
+
+    for (let button of buttons) {
+        button.addEventListener("click", calculate);
+    }
+
+
+
+    function calculate(event) {
+        let value = Number(event.target.parentElement.querySelector("input[type=text]").value);
+        let typeOfButton = event.target.id;
+
+        let daysValue = 0;
+
+        if (typeOfButton === "daysBtn") {
+            daysValue = value;
+        }
+        else if (typeOfButton === "hoursBtn") {
+            daysValue = value / 24;
+        }
+        else if (typeOfButton === "minutesBtn") {
+            daysValue = (value / 24) / 60;
+        }
+        else if (typeOfButton === "secondsBtn") {
+            daysValue = ((value / 24) / 60)/60;
+
+        }
+
+        let hoursValue = daysValue * 24;
+        let minutesValue = hoursValue * 60;
+        let secondsValue = minutesValue * 60;
+
+
+        document.getElementById("days").value = daysValue;
+        document.getElementById("hours").value = hoursValue;
+        document.getElementById("minutes").value = minutesValue;
+        document.getElementById("seconds").value = secondsValue;
+
+    }
+
+
 }
