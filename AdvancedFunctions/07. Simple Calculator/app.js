04824f079723c582521aa967807d98eb18d87903
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-02 17:17:51 +0300

    07. Simple Calculator

--- AdvancedFunctions/07. Simple Calculator/app.js
+++ AdvancedFunctions/07. Simple Calculator/app.js
@@ -1,7 +1,32 @@
 function calculator() {
-    // TODO:
+    let numberOne;
+    let numberTwo;
+    let result;
+
+    function init(selector1, selector2, resultOutput) {
+        numberOne = document.querySelector(selector1);
+        numberTwo = document.querySelector(selector2);
+        result = document.querySelector(resultOutput);
+    };
+
+    //add function
+    function add() {
+        result.value = Number(numberOne.value) + Number(numberTwo.value);
+    };
+
+    //subtract function
+    function subtract() {
+        result.value = Number(numberOne.value) - Number(numberTwo.value);
+    }
+
+    //completing the final object
+    let completedObject = {
+        init,
+        add,
+        subtract
+    };
+    return completedObject;
 }
 
-
-
-
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
