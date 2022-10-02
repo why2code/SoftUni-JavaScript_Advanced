228bb1c476a375337a9525dda406d3ce98df0b91
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-02 17:51:24 +0300

    08. Next Article

--- AdvancedFunctions/08. Next Article/script.js
+++ AdvancedFunctions/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let separateArticles = articles;
+
+    let showNext = function showNextArticleOnPage() {
+        
+        let contentElement = document.getElementById("content");
+        let newArtcl = document.createElement("article");
+
+        if (separateArticles.length > 0) {
+            newArtcl.textContent = separateArticles.shift();
+            contentElement.appendChild(newArtcl);
+        }
+    }
+
+    return showNext;
 }
