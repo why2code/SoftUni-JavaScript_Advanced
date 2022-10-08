6c6a236d9714dc1e87124f5e4178be4af6752098
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-08 16:34:20 +0300

    06. Dynamic Validation

--- UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
+++ UnitTestingAndErrorHandling/06.Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    // TODO
+    let inputField = document.getElementById("email");
+    inputField.addEventListener("change", verifyEmail);
+
+    let validateEmailRegex = /^[a-z]+\@[a-z]+.[a-z]+$/g;
+
+    function verifyEmail(){
+        let enteredEmail = inputField.value;
+
+        if(!enteredEmail.match(validateEmailRegex)){
+            inputField.classList.add("error");
+        }
+        else{
+            inputField.classList.remove("error");
+        }
+    }
 }
