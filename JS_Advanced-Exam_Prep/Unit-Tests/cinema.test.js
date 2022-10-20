ffb247058c5cdcbafe32dbd9e4d9b077fb56f833
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-20 23:21:07 +0300

    3. Unit Testing structure and Unit testing setup (Mocha/Chai)

---
+++ JS_Advanced-Exam_Prep/Unit-Tests/cinema.test.js
@@ -0,0 +1,14 @@
+const {cinema} = require("../03. Cinema/cinema.js");
+const {assert} = require("chai");
+
+describe("Tests …", function() {
+    describe("TODO …", () => {
+
+        it("TODO …", () => {
+             assert.equal(1,1);
+        });
+
+     });
+     
+      
+});
