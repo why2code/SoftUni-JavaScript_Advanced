db6addcac5e5a9faf70a6e1fd3cbafcdb729ce34
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-10-31 19:34:39 +0200

    01. Bus Stop

--- AsynchronousProgramming/01.Bus-Stop/app.js
+++ AsynchronousProgramming/01.Bus-Stop/app.js
@@ -1,11 +1,26 @@
 async function getInfo() {
     let idEl = document.getElementById("stopId");
+    let nameEl = document.getElementById("stopName");
+    let busesUl = document.getElementById("buses");
     let idValue = idEl.value;
 
     const url = `http://localhost:3030/jsonstore/bus/businfo/${idValue}`;
 
+   try{
     let response = await fetch(url);
     let data = await response.json();
     
-    debugger
+    nameEl.textContent = data.name;
+    idEl.value = "";
+    busesUl.innerHTML = "";
+
+    Object.entries(data.buses).forEach(([k,v]) =>{
+        let li = document.createElement("li");
+        li.textContent = `Bus ${k} arrives in ${v} minutes`
+        busesUl.appendChild(li);
+    });
+   } catch(e){
+    nameEl.textContent = "Error";
+   }
+
 }
