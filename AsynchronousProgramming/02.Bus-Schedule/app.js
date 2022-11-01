6824210e5516801bb804b84c71d4d64fcef594fc
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-11-01 22:26:17 +0200

    02. Bus Schedule

--- AsynchronousProgramming/02.Bus-Schedule/app.js
+++ AsynchronousProgramming/02.Bus-Schedule/app.js
@@ -1,11 +1,27 @@
 function solve() {
+    const resField = document.querySelector('.info');
+    const deptBtn = document.querySelector('#depart');
+    const arrBtn = document.querySelector('#arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stopId = 'depot';
+    let stopName = '';
+
+    async function depart() {
+        let info = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`);
+        let result = await info.json();
+        stopName = result.name;
+        resField.textContent = `Next stop ${stopName}`;
+        stopId = result.next;
+
+        deptBtn.disabled = true;
+        arrBtn.disabled = false;
     }
 
+    
     function arrive() {
-        console.log('Arrive TODO...');
+        resField.textContent = `Arriving at ${stopName}`;
+        deptBtn.disabled = false;
+        arrBtn.disabled = true;
     }
 
     return {
