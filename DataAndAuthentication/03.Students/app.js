d412f1e1ac3a2379176555f46c886cf0b83f5d39
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-11-05 14:49:51 +0200

    04. Book Library

--- DataAndAuthentication/03.Students/app.js
+++ DataAndAuthentication/03.Students/app.js
@@ -1,80 +1,45 @@
-async function init() {
-    let fnameField = document.querySelector("[name=firstName]");
-    let lnameField = document.querySelector("[name=lastName]");
-    let facultyNumber = document.querySelector("[name=facultyNumber]");
-    let gradeField = document.querySelector("[name=grade]");
+async function solve(e) {
+    const form = document.getElementById('form');
+    const table = document.querySelector('tbody');
+    const baseUrl = 'http://localhost:3030/jsonstore/collections/students';
 
-    let url = `http://localhost:3030/jsonstore/collections/students`;
-    let tableBodyOutput = document.getElementById("results").children[1];
-    document.getElementById("submit").addEventListener("click", dataSubmit);
+    form.addEventListener('submit', onSubmit);
 
+    table.innerHTML = '';
+    let response = await fetch(baseUrl);
+    let result = await response.json();
 
-    async function dataSubmit(e) {
+    let allStudents = Object.values(result);
+
+    allStudents.forEach(s => {
+        let row = document.createElement('tr');
+
+        for (const data in s) {
+            if (data == '_id') continue;
+
+            let cell = document.createElement('td');
+            cell.textContent = s[data];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    });
+
+    async function onSubmit(e) {
         e.preventDefault();
-        tableBodyOutput.innerHTML = "";
 
-        let fnValue = fnameField.value;
-        let lnValue = lnameField.value;
-        let fcValue = facultyNumber.value;
-        let grValue = gradeField.value;
+        let data = new FormData(form)
+        const studentObject = Object.fromEntries(data.entries());
 
-        if (!fnValue || !lnValue || !fcValue || !grValue) {
-            let response = await fetch(url);
-            let data = await response.json();
-            generateLineReport(data);
-        }
-        else{
-            let newStudent = {
-                firstName: fnValue,
-                lastName: lnValue,
-                facultyNumber: fcValue,
-                grade: grValue
-            }
-            let response = await fetch(url, {
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newStudent)
-            });
-            let result = response.json();
-
-            let updatedResponse = await fetch(url);
-            let newData = await updatedResponse.json();
-            generateLineReport(newData);
-
-            fnameField.value = "";
-            lnameField.value = "";
-            facultyNumber.value = "";
-            gradeField.value = "";
-            return {result, newData};
-        }
-    }
-
-    function generateLineReport(data) {
-        for (let student in data) {
-            let tr = document.createElement("tr");
-
-            let tdName = document.createElement("td");
-            tdName.textContent = data[student].firstName;
-
-            let tdLastName = document.createElement("td");
-            tdLastName.textContent = data[student].lastName;
-
-            let tdFaculty = document.createElement("td");
-            tdFaculty.textContent = data[student].facultyNumber;
-
-            let tdGrade = document.createElement("td");
-            tdGrade.textContent = Number(data[student].grade).toFixed(2);
-
-            tr.appendChild(tdName);
-            tr.appendChild(tdLastName);
-            tr.appendChild(tdFaculty);
-            tr.appendChild(tdGrade);
-            tableBodyOutput.appendChild(tr);
+        if (Object.values(studentObject).includes('')) {
+            return;
         }
 
+        await fetch(baseUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(studentObject)
+        });
     }
 }
 
-init();
+solve()
