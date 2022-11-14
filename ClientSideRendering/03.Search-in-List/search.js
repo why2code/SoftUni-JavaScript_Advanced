abe702d27f5f97fe9aa9ac0560ea8d66f61cffc2
Haralampi Stoyanov <futurefinancesolutions@gmail.com>
2022-11-14 20:12:50 +0200

    03. Search in List

--- ClientSideRendering/03.Search-in-List/search.js
+++ ClientSideRendering/03.Search-in-List/search.js
@@ -1,3 +1,28 @@
+import { render, html } from 'https://unpkg.com/lit-html?module';
+import { repeat } from 'https://unpkg.com/lit-html/directives/repeat.js?module';
+import { towns } from './towns.js';
+ 
+ 
+const townsDiv = document.getElementById('towns');
+const button = document.querySelector('button');
+button.addEventListener('click', search);
+const renderTowns = () => html`
+<ul> 
+   ${repeat(towns, town => html`<li>${town}</li>`)}
+</ul>`
+ 
+render(renderTowns(), townsDiv);
+ 
 function search() {
-   // TODO
+   const searchText = document.getElementById('searchText');
+   const townsList = townsDiv.querySelectorAll('li');
+   let count = 0;
+   townsList.forEach(t => {
+      t.classList.remove('active');
+      if (t.textContent.includes(searchText.value)) {
+         t.classList.add('active');
+         count++;
+      }
+   })
+   document.getElementById('result').textContent = `${count} matches found`;
 }
